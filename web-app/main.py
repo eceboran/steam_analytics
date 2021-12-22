5d05fc2f5ca77b530af62bcb49019dfe8571e15d
eceboran <49809557+eceboran@users.noreply.github.com>
2021-12-22 08:52:32 +0100

    updated requirements

--- web-app/main.py
+++ web-app/main.py
@@ -4,8 +4,7 @@ import os
 
 if __name__ == "__main__":
 #    app.run()
-    app.run(host="localhost", port=os.environ.get('PORT'), debug=True)
-#    app.run(host="0.0.0.0", port=os.environ.get('PORT'), debug=True)
+#    app.run(host="localhost", port=os.environ.get('PORT'), debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get('PORT'), debug=True)
 #    app.run(host="0.0.0.0", port=os.environ.get('PORT'), debug=True)
 #    app.run(host="localhost", port=5000, debug=True)
-# os.
