04b6e378ee40cc8d2e19f81116576635d6f3f81a
eceboran <49809557+eceboran@users.noreply.github.com>
2021-12-22 09:53:07 +0100

    updated hostip

--- web-app/hello_world.py
+++ web-app/hello_world.py
@@ -1,5 +1,11 @@
 import os
+import socket
+
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+
 print("Hello world!")
 print(os.environ.get('PORT'))
 print(os.listdir())
-print(os.listdir(os.path.join(os.path.abspath("."),'app')))
+print(os.listdir(os.path.join(os.path.abspath("."), 'app')))
+print(local_ip)
--- web-app/main.py
+++ web-app/main.py
@@ -4,7 +4,9 @@ import os
 
 if __name__ == "__main__":
 #    app.run()
-#    app.run(host="localhost", port=os.environ.get('PORT'), debug=True)
+#    app.run(host="localhost", port=os.environ.get('PORT'), debug=True)#
     app.run(host="0.0.0.0", port=os.environ.get('PORT'), debug=True)
+    app.run(host="192.168.1.7", port=os.environ.get('PORT'), debug=True)
+
 #    app.run(host="0.0.0.0", port=os.environ.get('PORT'), debug=True)
 #    app.run(host="localhost", port=5000, debug=True)
