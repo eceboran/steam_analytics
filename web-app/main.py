e52f577b83861587f18a63bb3fcba087a3b3d421
eceboran <49809557+eceboran@users.noreply.github.com>
2021-12-22 10:00:03 +0100

    updated ip

--- web-app/main.py
+++ web-app/main.py
@@ -1,12 +1,16 @@
 from app import app
 import os
+import socket
+
 
 
 if __name__ == "__main__":
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
 #    app.run()
 #    app.run(host="localhost", port=os.environ.get('PORT'), debug=True)#
-    app.run(host="0.0.0.0", port=os.environ.get('PORT'), debug=True)
-    app.run(host="192.168.1.7", port=os.environ.get('PORT'), debug=True)
-
+    app.run(host=local_ip, port=os.environ.get('PORT'), debug=True)
+#    app.run(host="0.0.0.0", port=os.environ.get('PORT'), debug=True)
+#    app.run(host="192.168.1.7", port=os.environ.get('PORT'), debug=True)
 #    app.run(host="0.0.0.0", port=os.environ.get('PORT'), debug=True)
 #    app.run(host="localhost", port=5000, debug=True)
