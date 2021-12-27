1f5004a8b793c2b615c921ee57becc43469afd0b
eceboran <49809557+eceboran@users.noreply.github.com>
2021-12-27 16:50:15 +0100

    added barplot

--- web-app/app/routes.py
+++ web-app/app/routes.py
@@ -1,10 +1,11 @@
 import plotly.utils
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, url_for, request
 from app import app
 import os
 import sqlite3
 import plotly.express as px
 import json
+import pandas as pd
 
 # import pandas as pd
 
@@ -24,7 +25,7 @@ def home():
     cursor = conn.cursor()
     # Top genres
     # no_top_genres = 12
-    sql_query = sql_query = f"""SELECT genre, COUNT(genre) as genre_count
+    sql_query = f"""SELECT genre, COUNT(genre) as genre_count
     FROM(
     SELECT gms.steam_appid, gnr.genre, gms_gnr.genre_id, gms.total_reviews, gms.price_initial
     FROM
@@ -35,7 +36,6 @@ def home():
     ORDER BY genre_count DESC
     LIMIT {no_top_genres}
     """
-    #sql_query = "SELECT * FROM Games"
     cursor.execute(sql_query)
     result = cursor.fetchall()
     genres = result
@@ -45,7 +45,38 @@ def home():
     fig = px.pie(values=values, names=names)
 
     graph1JSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template('home.html', no_top_genres=no_top_genres, games=genres, graph1JSON=graph1JSON)
+
+    sql_query = """SELECT gms.steam_appid, gnr.genre, gms_gnr.genre_id, gms.total_reviews, gms.price_initial, gms.review_score
+    FROM
+    games gms
+    JOIN GamesGenresRelation gms_gnr ON gms.steam_appid = gms_gnr.steam_appid
+    JOIN genres gnr ON gnr.genre_id = gms_gnr.genre_id
+    WHERE gnr.genre IN
+    (SELECT genre
+    FROM(
+    SELECT gms.steam_appid, gnr.genre, gms_gnr.genre_id, gms.total_reviews, gms.price_initial
+    FROM
+    games gms
+    JOIN GamesGenresRelation gms_gnr ON gms.steam_appid = gms_gnr.steam_appid
+    JOIN genres gnr ON gnr.genre_id = gms_gnr.genre_id)
+    GROUP BY genre
+    ORDER BY COUNT(genre) DESC
+    LIMIT 10)
+    """
+    cursor.execute(sql_query)
+    result = cursor.fetchall()
+    games_reviews = result
+
+    genre = [x[1] for x in games_reviews]
+    total_reviews = [x[5] for x in games_reviews]
+    df_genre_review = pd.DataFrame(genre, columns=["genre"])
+    df_genre_review["review_score"] = pd.Series(total_reviews)
+
+    fig = px.violin(df_genre_review, y="review_score", x="genre", color="genre")
+    graph2JSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('home.html', no_top_genres=no_top_genres, games=genres,
+                           graph1JSON=graph1JSON, graph2JSON=graph2JSON)
 
 
 @app.route('/game_details', methods=['GET', 'POST'])
